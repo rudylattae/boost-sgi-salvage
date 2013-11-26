468940547660aba624afab36b54714bc99c93487
Rudy Lattae <rudylattae@gmail.com>
2013-11-26 00:04:45 -0600

    Functional proof of concept

--- boostSgiSalvage.js
+++ boostSgiSalvage.js
@@ -1,8 +1,20 @@
+function preparetableForThumbnails( table ) {
+    table.find('thead tr').prepend('<th class="header">&nbsp;</th>');
+    table.find('tbody tr').each(function(i, row) {
+        var me = $(row);
+        if ( me.find('.thumb').length == 0 ) {
+            me.prepend('<td class="js-controls"></td>');            
+        }
+    });
+}
+
 function getItemSummariesFromTable( table ) {
     var itemSummaries = {};
 
     table.find('tbody tr').each(function(i, tableRow) { 
+        if (i >= 30) return;
         var summary = getItemSummaryFromRow( tableRow );
+        updateItemRowWithThumbnail( tableRow, summary );
         itemSummaries[summary.id] = summary;
     });
 
@@ -11,7 +23,7 @@ function getItemSummariesFromTable( table ) {
 
 
 function getItemSummaryFromRow( row ) {
-    var rawUrl = $('td:first a', row).attr('href'),
+    var rawUrl = $('td a', row).eq(1).attr('href'),
         detailsUrl = parseUrl(rawUrl),
         id = detailsUrl.params.stock_num
 
@@ -22,9 +34,26 @@ function getItemSummaryFromRow( row ) {
     };
 }
 
-var items = getItemSummariesFromTable($('#bid_items'));
+
+function updateItemRowWithThumbnail( row, summary ) {
+
+    $.get(summary.detailsUrl, function( data ){
+        var srcImage = $('img[name="image1"]', data),
+            a = $('<a class="thumb" target="_blank"></a>')
+                        .attr('href', summary.detailsUrl),
+            img = $('<img alt="loading..."/>')
+                        .attr('src', srcImage[0].src)
+                        .attr('width', 245)
+                        .appendTo(a);
+
+        $('.js-controls', row).append(a);
+    });
+}
 
 
+var t = $('#bid_items');
+preparetableForThumbnails( t );
+var items = getItemSummariesFromTable( t );
 
 
 // ====================
