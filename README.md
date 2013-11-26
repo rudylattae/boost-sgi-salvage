d7b4efe70489487f5c60de3c5d855b5e03999419
Rudy Lattae <rudylattae@gmail.com>
2013-11-25 22:07:11 -0600

    Add utilities. Improve docs

--- README.md
+++ README.md
@@ -9,12 +9,27 @@ on the client side (in the browser).
 Features
 ---------
 
+- [ ] Local "catalogue" of the items listed on the website
 - [ ] "Star" items you are interested in so you can keep an eye on them
 - [ ] Fast access to the items you have starred for instant review
 - [ ] Flow mode, for an album-like, big-picture view of items
 - [ ] Quickly see additional details of your starred items
 - [ ] Streamlined VIN lookup for vehicles
 
+
+Technical
+------------
+
+This project aims to augment the existing functionality and data that is available on the website. The 
+idea is that it merely get's injected into the page rendered by the site. After that, it's all Javascript and ajaxy 
+goodness! 
+
+Because on of the above, it is heavily dependent on:
+
+# the data and table structure presented by the website
+# additional services provided by SGI (e.g. VIN lookup)
+
+It also leverages the `jQuery` library used by the site. 
 
 
 Development
---
+++ boostSgiSalvage.js
@@ -0,0 +1,45 @@
+
+
+
+
+// ====================
+// Utilities
+// ====================
+
+// REF: http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function splitUrlParams(queryPart) {
+    if (typeof a === undefined ) return {};
+
+    queryPart = queryPart.split('?') ? queryPart.split('?')[1] : queryPart;
+
+    var a = queryPart.split('&'),
+        b = {},
+        i = 0;
+        limit = a.length;
+
+    for (; i < limit; ++i)
+    {
+        var p=a[i].split('=');
+        if (p.length != 2) continue;
+        b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+}
+
+// REF: http://stackoverflow.com/questions/736513/how-do-i-parse-a-url-into-hostname-and-path-in-javascript
+function parseUrl(url) {
+    var result = {},
+        anchor = document.createElement('a'),
+        keys = 'protocol hostname host pathname port search hash href'.split(' ');
+
+    anchor.href = url;
+    for (k in keys) {
+        var me = keys[k]; 
+        result[me] = anchor[me];
+    }
+
+    result.toString = function() { return anchor.href; };
+    result.requestUri = result.pathname + result.search;
+    result.params = splitUrlParams( result.search );
+    return result;
+};
