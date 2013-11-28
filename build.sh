7596e1731895f7c5919ca16b8cd0d66789ce57d3
Rudy Lattae <rudylattae@gmail.com>
2013-11-27 21:25:06 -0600

    Remove modjs related stuff. It did not work out.

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-package_name=boostSgiSalvage
-
-m cat main.js lib/utils.js lib/LocalDB.js -d build/$package_name.js
-m min build/$package_name.js
-m min bookmarklet.js -d build/$package_name-bookmarklet.js
